dcec56be746232ec8edc8342d15ef7c65824fc27
e17gomes <e17gomes@gmail.com>
2024-11-10 23:47:03 -0300

    feat: add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,31 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {Inter} from "next/font/google"
-const inter = Inter({subsets:['latin']})
-import {ThemeProvider} from "@/app/providers/theme-provider"
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/app/providers/theme-provider";
 
 export const metadata: Metadata = {
-  title: "Sone",
-  description: "This is SONE",
+	title: "Sone",
+	description: "This is SONE",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        <ThemeProvider
-        attribute={"class"}
-        defaultTheme={"system"}
-        enableSystem
-        disableTransitionOnChange
-        >
-        {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${inter.className} antialiased`}>
+				<ThemeProvider
+					attribute={"class"}
+					defaultTheme={"system"}
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children} 
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- src/app/providers/theme-provider.tsx
+++ src/app/providers/theme-provider.tsx
@@ -1,10 +1,9 @@
-import * as React from "react"
-import {ThemeProvider as NextThemeProvider} from "next-themes"
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import type * as React from "react";
 
-export function ThemeProvider({children, ...props}: React.ComponentProps<typeof NextThemeProvider>){
-    return(
-        <NextThemeProvider {...props}>
-            {children}
-        </NextThemeProvider>
-    )
+export function ThemeProvider({
+	children,
+	...props
+}: React.ComponentProps<typeof NextThemeProvider>) {
+	return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 }
